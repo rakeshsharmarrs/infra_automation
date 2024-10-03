1e23388020a824219d18e915c19e8f57d80bfe37
rakeshsharmarrs <123501507+rakeshsharmarrs@users.noreply.github.com>
2024-10-03 06:09:49 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "kubernetes" {
-  host                   = var.ocp_url
-  token                  = var.ocp_token
-  cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
+  host  = var.ocp_url
+  token = var.ocp_token
 
   load_config_file = false  # Disable automatic kubeconfig loading
 }
@@ -51,30 +50,4 @@ resource "kubernetes_deployment" "nginx_deployment" {
             requests {
               memory = "256Mi"
               cpu    = "250m"
-            }
-          }
-        }
-      }
-    }
-  }
-}
-
-resource "kubernetes_service" "nginx_service" {
-  metadata {
-    name      = "nginx-service"
-    namespace = kubernetes_namespace.nginx_namespace.metadata[0].name
-  }
-
-  spec {
-    selector = {
-      app = "nginx"
-    }
-
-    port {
-      port        = 80
-      target_port = 80
-    }
-
-    type = "ClusterIP"
-  }
-}
+        
